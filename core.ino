28f8f79a8662ca768fe1dd26348eabfd84f72e20
Pavich Komansil <phavit_k@hotmail.com>
2024-09-12 17:15:09 +0700

    Update core.ino

--- core.ino
+++ core.ino
@@ -1,26 +1,5 @@
 #include <IBusBM.h>
 
-// (INCOMPLETED) Define motor driver pin (ex. m1f, m1 = Motor 1, f= Forward, or b = Backward, sp = Speed).
-//m1-m3 are wheels and m4 is lift.
-
-/* UNCOMMENT when FINISED.
-#define m1f 
-#define m1b 
-#define m1sp 5
-
-#define m2f 
-#define m2b 
-#define m2sp 4
-
-#define m3f
-#define m3b
-#define m3sp 33
-
-#define m4f
-#define m4b
-#define m4sp 25
-*/
-
 // iBus 1.1.4 (FS-I6X)
 
 IBusBM ibus; //iBus Object
@@ -38,29 +17,22 @@ bool readSwitch(byte channelInput, bool defaultValue) {
   int ch = readChannel(channelInput, 0, 100, intDefaultValue);
   return (ch > 50);
 }
- 
+
 void setup() {
   Serial.begin(115200);
   ibus.begin(Serial1); //iBus init on Serial1
 }
- 
+
 void loop() {
 
   //INPUT Zone...
 
-  //CH1-5 (Joy)
-  int CH1value = readChannel(0, -100, 100, 0);
-  int CH2value = readChannel(1, -100, 100, 0);
-  int CH3value = readChannel(2, -100, 100, 0);
-  int CH4value = readChannel(3, -100, 100, 0);
-  int CH5value = readChannel(4, -100, 100, 0);
-  Serial.print(CH1value);
-
-  //CH6 (Switch)
-  int CH6value = readSwitch(5, false);
-  Serial.print(CH6value);
-
-  Serial.println();
+  //CH1-4 (Joy)
+  int ROTATE = readChannel(0, -100, 100, 0);
+  int LIFT = readChannel(1, -100, 100, 0);
+  int FB = readChannel(2, -100, 100, 0);
+  int LR = readChannel(3, -100, 100, 0);
+  //Small delay
   delay(10);
 
   //End INPUT Zone...
