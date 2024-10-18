1e0e6f06df4656b4831d217218d8413bc5377df3
Pavich Komansil <phavit_k@hotmail.com>
2024-10-18 15:37:22 +0700

    robot will stop when receiver disconnect.

--- core.ino
+++ core.ino
@@ -52,66 +52,71 @@ void setup() {
 }
 
 void loop() {
-
-  //CH1-4 (Joy)
-  int rawROTATE = readChannel(0, -100, 100, 0);
-  int rawFB = readChannel(2, -100, 100, 0);
-  int rawLR = 0; //readChannel(3, -100, 100, 0);
+  if (Serial1.available()){
+    //CH1-4 (Joy)
+    int rawROTATE = readChannel(0, -100, 100, 0);
+    int rawFB = readChannel(2, -100, 100, 0);
+    int rawLR = 0; //readChannel(3, -100, 100, 0);
   
-  //Mapping
-  if(rawROTATE<-20){
-    int ROTATE_CCW = 0;
-    ROTATE_CCW = map(rawROTATE, 0, -100, 0, 80);
+    //Mapping
+    if(rawROTATE<-20){
+      int ROTATE_CCW = 0;
+      ROTATE_CCW = map(rawROTATE, 0, -100, 0, 80);
 
-    digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,ROTATE_CCW);
-    digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,ROTATE_CCW);
-    digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,ROTATE_CCW);
+      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,ROTATE_CCW);
+      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,ROTATE_CCW);
+      digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,ROTATE_CCW);
 
-  }else if(rawROTATE>=20){
-    int ROTATE_CW = 0;
-    ROTATE_CW = map(rawROTATE, 0, 100, 0, 80);
+    }else if(rawROTATE>=20){
+      int ROTATE_CW = 0;
+      ROTATE_CW = map(rawROTATE, 0, 100, 0, 80);
 
-    digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,ROTATE_CW);
-    digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,ROTATE_CW);
-    digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,ROTATE_CW);
+      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,ROTATE_CW);
+      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,ROTATE_CW);
+      digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,ROTATE_CW);
 
-  }else if(rawFB<-20){
-    int FW = 0;
-    FW = map(rawFB, 0, -100, 0, 220);
+    }else if(rawFB<-20){
+      int FW = 0;
+      FW = map(rawFB, 0, -100, 0, 220);
     
-    digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,FW);
-    digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,FW);
-    digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
+      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,FW);
+      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,FW);
+      digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
 
-  }else if(rawFB>=20){
-    int RW = 0;
-    RW = map(rawFB, 0, 100, 0, 220);
+    }else if(rawFB>=20){
+      int RW = 0;
+      RW = map(rawFB, 0, 100, 0, 220);
 
-    digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,RW);
-    digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,RW);
-    digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
+      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,RW);
+      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,RW);
+      digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
 
-  }else if(rawLR<-20){
-    /* (Disabled)
-    int L = 0;
-    L = map(rawLR, 0, -100, 0, 200);
+    }else if(rawLR<-20){
+      /* (Disabled)
+      int L = 0;
+      L = map(rawLR, 0, -100, 0, 200);
 
-    digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,L);
-    digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,L);
-    digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,L);
-    */
-  }else if(rawLR>=20){
-    /* (Disabled)
-    int R = 0;
-    R = map(rawLR, 0, 100, 0, 200);
+      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,L);
+      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,L);
+      digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,L);
+      */
+    }else if(rawLR>=20){
+      /* (Disabled)
+      int R = 0;
+      R = map(rawLR, 0, 100, 0, 200);
 
-    digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,R);
-    digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,R);
-    digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,R);
-    */
+      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,R);
+      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,R);
+      digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,R);
+      */
+    }else{
+      digitalWrite(f1,0);digitalWrite(b1,0);analogWrite(s1,0);
+      digitalWrite(f2,0);digitalWrite(b2,0);analogWrite(s2,0);
+      digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
+    } 
   }else{
     digitalWrite(f1,0);digitalWrite(b1,0);analogWrite(s1,0);
     digitalWrite(f2,0);digitalWrite(b2,0);analogWrite(s2,0);
     digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
-  } 
+  }
 }
