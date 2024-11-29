6444b7b722676faa576d3ed66d6d3a81d4d2ca6f
Pavich Komansil <phavit_k@hotmail.com>
2024-11-29 18:19:55 +0700

    more improvements

--- core.ino
+++ core.ino
@@ -2,9 +2,9 @@
 // iBus 1.1.4 (FS-I6X)
 
 //Motor 1 FL
-#define f2 2
-#define b2 3
-#define s2 4
+#define f2 13
+#define b2 12
+#define s2 11
 
 //Motor 2 FR
 #define f3 6
@@ -25,34 +25,18 @@ int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue)
   return map(ch, 1000, 2000, minLimit, maxLimit);
 }
 
-//Channel 6 reading (Switch) (Disabled)
-/*
-bool readSwitch(byte channelInput, bool defaultValue) {
-  int intDefaultValue = (defaultValue) ? 100 : 0;
-  int ch = readChannel(channelInput, 0, 100, intDefaultValue);
-  return (ch > 50);
-}
-*/
-
 void setup() {
   Serial.begin(115200);
-  ibus.begin(Serial1); //iBus init on Serial1
+  ibus.begin(Serial); //iBus init on Serial1
 
   //Set motor driver pinmode
-  for(int i=2;i<=10;i++){
+  for(int i=2;i<=13;i++){
     pinMode(i,OUTPUT);
   }
-
-  //Constant Speed (Disabled)
-  /*
-  analogWrite(s1,255);
-  analogWrite(s2,255);
-  analogWrite(s3,255);
-  */
 }
 
 void loop() {
-  if (Serial1.available()){
+  if (Serial.available()){
     //CH1-4 (Joy)
     int rawROTATE = readChannel(0, -100, 100, 0);
     int rawFB = readChannel(2, -100, 100, 0);
@@ -61,23 +45,23 @@ void loop() {
     //Mapping
     if(rawROTATE<-20){
       int ROTATE_CCW = 0;
-      ROTATE_CCW = map(rawROTATE, 0, -100, 0, 80);
+      ROTATE_CCW = map(rawROTATE, 0, -100, 0, 255);
 
-      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,ROTATE_CCW);
-      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,ROTATE_CCW);
-      digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,ROTATE_CCW);
+      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,ROTATE_CCW*0.5);
+      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,ROTATE_CCW*0.5);
+      digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,ROTATE_CCW*0.5);
 
     }else if(rawROTATE>=20){
       int ROTATE_CW = 0;
-      ROTATE_CW = map(rawROTATE, 0, 100, 0, 80);
+      ROTATE_CW = map(rawROTATE, 0, 100, 0, 255);
 
-      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,ROTATE_CW);
-      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,ROTATE_CW);
-      digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,ROTATE_CW);
+      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,ROTATE_CW*0.5);
+      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,ROTATE_CW*0.5);
+      digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,ROTATE_CW*0.5);
 
     }else if(rawFB<-20){
       int FW = 0;
-      FW = map(rawFB, 0, -100, 0, 220);
+      FW = map(rawFB, 0, -100, 0, 255);
     
       digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,FW);
       digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,FW);
@@ -85,30 +69,12 @@ void loop() {
 
     }else if(rawFB>=20){
       int RW = 0;
-      RW = map(rawFB, 0, 100, 0, 220);
+      RW = map(rawFB, 0, 100, 0, 255);
 
       digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,RW);
       digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,RW);
       digitalWrite(f3,0);digitalWrite(b3,0);analogWrite(s3,0);
 
-    }else if(rawLR<-20){
-      /* (Disabled)
-      int L = 0;
-      L = map(rawLR, 0, -100, 0, 200);
-
-      digitalWrite(f1,1);digitalWrite(b1,0);analogWrite(s1,L);
-      digitalWrite(f2,1);digitalWrite(b2,0);analogWrite(s2,L);
-      digitalWrite(f3,1);digitalWrite(b3,0);analogWrite(s3,L);
-      */
-    }else if(rawLR>=20){
-      /* (Disabled)
-      int R = 0;
-      R = map(rawLR, 0, 100, 0, 200);
-
-      digitalWrite(f1,0);digitalWrite(b1,1);analogWrite(s1,R);
-      digitalWrite(f2,0);digitalWrite(b2,1);analogWrite(s2,R);
-      digitalWrite(f3,0);digitalWrite(b3,1);analogWrite(s3,R);
-      */
     }else{
       digitalWrite(f1,0);digitalWrite(b1,0);analogWrite(s1,0);
       digitalWrite(f2,0);digitalWrite(b2,0);analogWrite(s2,0);
